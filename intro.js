83136624f696e5b4410a4f09fd7f014d54ba6daa
Daniel Mecke <daniel.mecke@cunningsoft.de>
2017-11-28 21:49:14 +0100

    fixed _getOffset() for nested elements with border and position relative

--- intro.js
+++ intro.js
@@ -2104,32 +2104,11 @@
     var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
     var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
 
-    if (element instanceof SVGElement) {
-      var x = element.getBoundingClientRect();
-      elementPosition.top = x.top + scrollTop;
-      elementPosition.width = x.width;
-      elementPosition.height = x.height;
-      elementPosition.left = x.left + scrollLeft;
-    } else {
-      //set width
-      elementPosition.width = element.offsetWidth;
-
-      //set height
-      elementPosition.height = element.offsetHeight;
-
-      //calculate element top and left
-      var _x = 0;
-      var _y = 0;
-      while (element && !isNaN(element.offsetLeft) && !isNaN(element.offsetTop)) {
-        _x += element.offsetLeft;
-        _y += element.offsetTop;
-        element = element.offsetParent;
-      }
-      //set top
-      elementPosition.top = _y;
-      //set left
-      elementPosition.left = _x;
-    }
+    var x = element.getBoundingClientRect();
+    elementPosition.top = x.top + scrollTop;
+    elementPosition.width = x.width;
+    elementPosition.height = x.height;
+    elementPosition.left = x.left + scrollLeft;
 
     return elementPosition;
   }
